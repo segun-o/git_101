4e936f4a6b553ff53a72de94384d7f232829e13d
Oluwasegun <oluwasegunodunlami3@gmail.com>
2025-11-08 07:49:52 +0100

    Added name parameter

--- second_file.py
+++ second_file.py
@@ -1,7 +1,7 @@
 def print_hello_world():
 	print("Hello World")
 
-def print_my_name():
-	print("Hi! My name is Segun")
+def print_my_name(name):
+	print(f"Hi! My name is {name}")
 
 print_hello_world()
